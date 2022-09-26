0e5700e6a49566a223596d5fa37bd225d205c6eb
arvindthoppe <tsarvind996@gmail.com>
2022-09-26 13:17:03 +0400

    updated about

--- app/components/About/index.jsx
+++ app/components/About/index.jsx
@@ -2,14 +2,15 @@ export const About = () => {
   return (
     <div className="w-full font-sans  text-gray-800 ">
       <h2 className="mt-2 mb-2 text-3xl font-medium">Full Stack Developer</h2>
-      <h6 className="mt-2 mb-2 text-xl ">Chennai,India</h6>
+      <h6 className="mt-2 mb-2 text-xl ">Madurai,India</h6>
       <p className="mt-2 mb-2 text-gray-500">
-        I build client-side of apps with modern web technologies,frameworks and
-        hoping to be part of a team or individually contribute to products that
-        help people/businesses solve their issues. In my previous role, I also
-        had the opportunity to gain experience in the field of Information
-        security and compliance that has helped me understand about different
-        security standards that organisations pursue.
+        Hey there! Thanks for visiting me :) I believe that technology has the
+        power to create stuff that has postive impact on everything around us.
+        This has helped me in my pursuit to be a software engineer and build web
+        applications with modern web technologies and frameworks. Always
+        enthusiatic about solving problems,chat about software practices and to
+        be part of a team or individually contribute to products. Moreover,I
+        enjoy the process and the tingle that it brings along.
       </p>
     </div>
   );
